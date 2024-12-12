a5182a2dd13f7d2ccf7600631980b7608bf6a8f9
Efdal Sancak <e.sancak@resalepartners.nl>
2024-12-12 12:52:14 +0100

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,3 @@
 pub use human::{Female, Man, EchoPerson};
-pub use http_client::errors;
-
 pub mod human;
 pub mod http_client;
-
-pub fn yolo(){
-    let a = Man{name: "yolo"};
-    let b = a.echo();
-
-    println!("Welcome to Echo {b}")
-}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use std::{rc::Rc, sync::atomic::{AtomicU64, Ordering::Relaxed}};
+use std::{sync::{atomic::{AtomicU64, Ordering::Relaxed}, mpsc::{channel, SendError, Sender}}, thread};
 
 
 trait RateLimiter: Send + Sync {
     fn did_exceed(&self) -> bool;
     fn count_up(&self);
+    fn notify(&self) -> Result<(), SendError<&str>>;
 }
 
 struct RateLimit<'a> {
     uid: &'a str,
+    max: u64,
     count: AtomicU64,
-    max: u64
+    notifier: Sender<&'a str>
 }
 
 impl RateLimiter for RateLimit<'_> {
@@ -20,16 +22,31 @@ impl RateLimiter for RateLimit<'_> {
     fn did_exceed(&self) -> bool {
         self.count.load(Relaxed) > self.max
     }
+
+    fn notify(&self) -> Result<(), SendError<&str>>{
+        self.notifier.send(self.uid)?;
+        Ok(())
+    }
 }
 
-fn send_and_sync(safe: impl RateLimiter + 'static) {}
+
+fn send_and_sync(safe: impl RateLimiter) {
+    safe.notify().unwrap();
+}
 
 fn main() {
+    let (sender, receiver) = channel::<&str>();
+
     let limit = RateLimit{
-        uid: "",
+        uid: "1234",
         count: AtomicU64::new(0),
-        max: 200
+        max: 200,
+        notifier: sender.clone()
     };
 
-    send_and_sync(limit);
+    thread::spawn(move || {
+        send_and_sync(limit);
+    });
+
+    println!("lets notify user[id: {}] about the rate limit!", receiver.recv().unwrap());
 }

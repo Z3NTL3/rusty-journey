0ab3750fe8b6608a56956b97b3a2adbefdfe68bd
Efdal Sancak <e.sancak@resalepartners.nl>
2025-01-06 14:33:46 +0100

    .

--- crates/webapp/src/lib.rs
+++ crates/webapp/src/lib.rs
@@ -18,7 +18,7 @@ pub trait WhoisResolver: Sized {
     type Error;
     fn new(opt: WhoisOpt) -> Whois;
     fn query(&self) -> impl Future<Output = Result<WhoisICANN, Self::Error>>;
-    fn lookup(&self, whois_server: &str, domain2_lookup: &str) -> impl Future<Output = Result<String, Self::Error>>;
+    fn lookup(whois_server: &str, domain2_lookup: &str) -> impl Future<Output = Result<String, Self::Error>>;
 }
 
 impl WhoisResolver for Whois {
@@ -28,7 +28,7 @@ impl WhoisResolver for Whois {
     }
     
     async fn query(&self) -> Result<WhoisICANN, Self::Error> {
-        let q1 = self.lookup(self.target.whois_server, self.target.domain2lookup).await?;
+        let q1 = <Whois as WhoisResolver>::lookup(self.target.whois_server, self.target.domain2lookup).await?;
         let main_server = 
         if let Some((_, b)) = q1.split_once("whois:") {
             b.trim().split_once("\n").ok_or_else(|| errors::WhoisError::MissingNewline)?.0
@@ -37,10 +37,10 @@ impl WhoisResolver for Whois {
         let port: &str = self.target.whois_server.split_once(":").ok_or_else(|| {
             Box::new(errors::WhoisError::GeneralErr{ ctx: "whois server parameter should be a host:port" })
         })?.1;
-        Ok(self.lookup(&format!("{main_server}:{port}"), self.target.domain2lookup).await?)
+        Ok(<Whois as WhoisResolver>::lookup(&format!("{main_server}:{port}"), self.target.domain2lookup).await?)
     }
 
-    async fn lookup(&self, whois_server: &str, domain2_lookup: &str) -> Result<String, Self::Error> {
+    async fn lookup(whois_server: &str, domain2_lookup: &str) -> Result<String, Self::Error> {
         let mut conn = TcpStream::connect(whois_server).await?;
         conn.write(format!("{domain2_lookup}\r\n").as_bytes()).await?;
         

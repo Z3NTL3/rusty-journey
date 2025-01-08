fa24fbe7408e3e75319b3b52fa3dcd3103e965e0
Efdal Sancak <e.sancak@resalepartners.nl>
2025-01-08 12:54:46 +0100

    .

--- crates/webapp/src/parser.rs
+++ crates/webapp/src/parser.rs
@@ -3,102 +3,65 @@ use std::{fmt::Debug, str::FromStr};
 pub use chrono::{DateTime, Utc};
 
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct WhoisInformation {
-    pub domain_name: String,
-    pub registry_domain_id: String,
-    pub registrar_whois_server: String,
-    pub registrar_url: String,
-    pub updated_date: DateTime<Utc>, // whois datetimes are expressed in UTC
-    pub creation_date: DateTime<Utc>,
-    pub registry_expirity_date: DateTime<Utc>,
-    pub registrar: String,
-    pub registrar_iana_id: String,
-    pub registrar_abuse_email_contact: String,
-    pub registrar_abuse_phone_contact: String,
-    pub domain_status: String,
-    pub nameservers: Vec<String>,
-    pub dnssec: String,
-}
-
-impl Default for WhoisInformation {
-    fn default() -> Self {
-        let default_time: DateTime<Utc> =  DateTime::default();
-        Self { 
-            domain_name: "".into(), 
-            registry_domain_id: "".into(), 
-            registrar_whois_server: "".into(), 
-            registrar_url: "".into(), 
-            updated_date: default_time, 
-            creation_date: default_time, 
-            registry_expirity_date: default_time, 
-            registrar: "".into(), 
-            registrar_iana_id: "".into(),
-            registrar_abuse_email_contact: "".into(), 
-            registrar_abuse_phone_contact: "".into(), 
-            domain_status: "".into(), 
-            nameservers: Vec::default(), 
-            dnssec: "" .into()
-        }
-    }
+    pub domain_name: Option<String>,
+    pub registry_domain_id: Option<String>,
+    pub registrar_whois_server: Option<String>,
+    pub registrar_url: Option<String>,
+    pub updated_date: Option<DateTime<Utc>>, // whois datetimes are expressed in UTC
+    pub creation_date: Option<DateTime<Utc>>,
+    pub registry_expirity_date: Option<DateTime<Utc>>,
+    pub registrar: Option<String>,
+    pub registrar_iana_id: Option<String>,
+    pub registrar_abuse_email_contact: Option<String>,
+    pub registrar_abuse_phone_contact: Option<String>,
+    pub domain_status: Option<String>,
+    pub name_servers: Option<Vec<String>>,
+    pub dnssec: Option<String>,
 }
 
 pub struct Parser;
 
 impl Parser {
+    // Creates a new parser
     pub fn new() -> Parser {
         Parser
     }
+    
+    // Parses a WHOIS information from a String into a WhoisInformation struct
     pub fn parse(&self, content: String) -> Result<WhoisInformation, Box<dyn std::error::Error>> {
-        let body = content.split("\n");
-        let mut parsed = WhoisInformation::default();
-        
-        let body = body.map(|item| {
-            let find =  item.trim().to_lowercase();
-            let joints = find.split_once(":").unwrap_or_default();
+        let lines = content.split("\n").flat_map(|line| line.split_once(":"));
+        let mut whois_information = WhoisInformation::default();
 
-            if find.contains(&joints.0) {
-                Some((joints.0.to_owned(), joints.1.to_owned()))
-            } else {
-                None
-            }
-        });
-
-        for item in body {
-            match item {
-                Some((key, value)) => {
-                    // println!("key: {key} - value: {value}");
-
-                    match key.as_str() {
-                        "domain name" => parsed.domain_name = value.into(),
-                        "registry domain id" => parsed.registry_domain_id = value.into(),
-                        "registrar whois server" => parsed.registrar_whois_server = value.into(),
-                        "registrar url" => parsed.registrar_url = value.into(),
-                        "updated date" => parsed.updated_date = match DateTime::<Utc>::from_str(value.as_str()) {
-                            Ok(date) => date,
-                            Err(err) => return Err(Box::new(err)),
-                        },
-                        "creation date" => parsed.creation_date = match DateTime::<Utc>::from_str(value.as_str()) {
-                            Ok(date) => date,
-                            Err(err) => return Err(Box::new(err)),
-                        },
-                        "registry expiry date" => parsed.registry_expirity_date = match DateTime::<Utc>::from_str(value.as_str()) {
-                            Ok(date) => date,
-                            Err(err) => return Err(Box::new(err)),
-                        },
-                        "registrar" => parsed.registrar = value.into(),
-                        "registrar iana id" => parsed.registrar_iana_id = value.into(),
-                        "registrar abuse contact email" => parsed.registrar_abuse_email_contact = value.into(),
-                        "registrar abuse contact phone" => parsed.registrar_abuse_phone_contact = value,
-                        "domain status" => parsed.domain_status = value.into(),
-                        "name server" => parsed.nameservers.push(value),
-                        "dnssec" => parsed.dnssec = value,
-                        _ => ()
-                    }
+        for (key, value) in lines {
+            let key = key.trim();
+            let value = value.trim();
+            
+            match key.to_lowercase().as_str() {
+                "domain name" => whois_information.domain_name = Some(value.to_owned()),
+                "registry domain id" => whois_information.registry_domain_id = Some(value.to_owned()),
+                "registrar whois server" => {
+                    whois_information.registrar_whois_server = Some(value.to_owned())
+                }
+                "registrar url" => whois_information.registrar_url = Some(value.to_owned()),
+                "updated date" => whois_information.updated_date = Some(DateTime::<Utc>::from_str(value)?),
+            
+                "creation date" => whois_information.creation_date = Some(DateTime::<Utc>::from_str(value)?),
+                "registry expiry date" => whois_information.registry_expirity_date = Some(DateTime::<Utc>::from_str(value)?),
+                "registrar" => whois_information.registrar = Some(value.to_owned()),
+                "registrar iana id" => whois_information.registrar_iana_id = Some(value.to_owned()),
+                "registrar abuse contact email" => whois_information.registrar_abuse_email_contact = Some(value.to_owned()),
+                "registrar abuse contact phone" => whois_information.registrar_abuse_phone_contact = Some(value.to_owned()),
+                "domain status" => whois_information.domain_status = Some(value.to_owned()),
+                "name server" => match whois_information.name_servers.as_mut() {
+                    Some(name_servers) => name_servers.push(value.to_owned()),
+                    None => whois_information.name_servers = Some(Vec::new()),
                 },
-                None => (),
-            };
+                "dnssec" => whois_information.dnssec = Some(value.to_owned()),
+                _ => {}
+            }
         }
-        Ok(parsed)
+        Ok(whois_information)
     }
 }
